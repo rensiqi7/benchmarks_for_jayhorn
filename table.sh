508c1230ec63dcf4567ddc92dcdf51c458e4d456
shiki <rensiqi384906149@yahoo.co.jp>
2018-08-13 14:56:57 +0900

    add table

---
+++ table.sh
@@ -0,0 +1,32 @@
+
+#!/bin/bash
+
+cd algorithms
+
+echo "Running the algorithms benchmarks"
+for file in */ ; do
+  if [[ -d "$file" && ! -L "$file" ]]; then
+    echo "Running CBMC benchmark $file";
+    time java -jar ../../jayhorn.jar -j $file -t 60
+  fi;
+done
+
+'''
+cd ../svcomp_rec
+echo "Running the SVCOMP benchmarks"
+for file in */ ; do
+  if [[ -d "$file" && ! -L "$file" ]]; then
+    echo "Running SVCOMP benchmark $file";
+    time java -jar ../../jayhorn.jar -j $file -t 60
+  fi;
+done
+
+cd ../MinePump/spec1-5/
+echo "Running the MinePump benchmarks"
+for file in */ ; do
+  if [[ -d "$file" && ! -L "$file" ]]; then
+    echo "Running MinePump benchmark $file";
+    time java -jar ../../../jayhorn.jar -j $file -t 60
+  fi;
+done
+'''
